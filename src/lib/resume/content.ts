e53d63593bad865cdcc5f8267329e52b33ec28c8
Jordan Toering <toering3@gmail.com>
2024-07-19 16:34:17 -0700

    Refactor resume details into separate file and import

---
+++ src/lib/resume/content.ts
@@ -0,0 +1,124 @@
+export interface ResumeContent {
+	full_name: string;
+	profession_title: string;
+	location: string;
+	github_details: {
+		short_link: string;
+		full_url: string;
+	};
+	email: string;
+	work_experience_items: WorkExperienceItem[];
+	education_items: EducationItem[];
+	skill_items: string[];
+}
+
+export interface WorkExperienceItem {
+	job_title: string;
+	company_name: string;
+	date_year_start: string;
+	date_month_start: string;
+	date_year_end: string;
+	date_month_end: string;
+	description_items: string[];
+}
+
+export interface EducationItem {
+	degree_title: string;
+	school_name: string;
+	date_year_start: string;
+	date_year_end: string;
+}
+
+export const resumeContent: ResumeContent = {
+	full_name: 'Jordan Toering',
+	profession_title: 'Software Developer',
+	location: 'BC, Canada',
+	github_details: {
+		short_link: 'github.com/jordantoering',
+		full_url: 'https://github.com/jordantoering'
+	},
+	email: 'toering3@gmail.com',
+	work_experience_items: [
+		{
+			job_title: 'Full Stack Developer',
+			company_name: 'Elevated Signals',
+			date_year_start: '2021',
+			date_month_start: 'Apr',
+			date_year_end: '2024',
+			date_month_end: 'Apr',
+
+			description_items: [
+				'Improved Postgres database stability and reduced load across all instances by 80% through performance monitoring, query plan analysis, query optimization and index modification',
+				'Increased overall system responsiveness up to 5x through Express API endpoint refactoring and streamlining of data needed by Angular and React frontends',
+				'Added new tests and maintained existing test suites in Jest to increase confidence in new releases',
+				'Collaborated with product team to design new features, improve existing workflows and address customer needs',
+				'Communicated with customer support team for triage and resolution of problems reported in production',
+				'Reviewed pull requests and offered constructive feedback for other members of development team'
+			]
+		},
+		{
+			job_title: 'Software Developer',
+			company_name: 'TELUS',
+			date_year_start: '2019',
+			date_month_start: 'Sept',
+			date_year_end: '2021',
+			date_month_end: 'Apr',
+			description_items: [
+				'Used Express/Nodejs, React and Cassandra in development of a full stack internal web application to enable self-serve interaction between TELUS team members and a large-scale vendor API ecosystem using Kong',
+				'Developed, deployed and supported microservice APIs defined with OAS2 using Express/Nodejs, Jest, and the OpenShift container platform',
+				'Mentored junior developers in system design, code quality and critical thinking to improve team productivity',
+				'Published a privately scoped NPM package to facilitate standardization of request/response logging and correlation id implementations in other Express/Nodejs microservices'
+			]
+		},
+		{
+			job_title: 'Software Developer',
+			company_name: 'TELUS',
+			date_year_start: '2016',
+			date_month_start: 'Sep',
+			date_year_end: '2017',
+			date_month_end: 'Apr',
+			description_items: [
+				'Developed rich web applications with JavaScript and AngularJS to increase visibility and monitoring potential of the company’s software services',
+				'Wrote backend scripts with PHP and SQL to collect and process service status data to better understand downtime events and causes'
+			]
+		},
+		{
+			job_title: 'Software Developer in Test',
+			company_name: 'Pulse Energy/EnerNOC',
+			date_year_start: '2014',
+			date_month_start: 'Sep',
+			date_year_end: '2015',
+			date_month_end: 'Apr',
+			description_items: [
+				'Performed regression testing prior to product releases to guard against bugs in the production environment',
+				'Wrote web automation tests with Selenium and Python to reduce manual testing load and promote rapid smoke-testing of the platform'
+			]
+		}
+	],
+	education_items: [
+		{
+			degree_title: 'BSc. Computer Science',
+			school_name: 'Simon Fraser University',
+			date_year_start: '2012',
+			date_year_end: '2018'
+		}
+	],
+	skill_items: [
+		'Typescript',
+		'Javascript',
+		'ExpressJS',
+		'Angular',
+		'React',
+		'Postgres',
+		'SQL',
+		'AWS',
+		'Git',
+		'Jest',
+		'HTML',
+		'CSS',
+		'OAuth2',
+		'OpenAPI',
+		'API Design',
+		'i18n'
+	]
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,105 +1,20 @@
 <script>
-	const positionItems = [
-		{
-			job_title: 'Full Stack Developer',
-			company: 'Elevated Signals',
-			date_year_start: '2021',
-			date_month_start: 'Apr',
-			date_year_end: '2024',
-			date_month_end: 'Apr',
-			currently_working: false,
-			description_items: [
-				'Improved Postgres database stability and reduced load across all instances by 80% through performance monitoring, query plan analysis, query optimization and index modification',
-				'Increased overall system responsiveness up to 5x through Express API endpoint refactoring and streamlining of data needed by Angular and React frontends',
-				'Added new tests and maintained existing test suites in Jest to increase confidence in new releases',
-				'Collaborated with product team to design new features, improve existing workflows and address customer needs',
-				'Communicated with customer support team for triage and resolution of problems reported in production',
-				'Reviewed pull requests and offered constructive feedback for other members of development team'
-			]
-		},
-		{
-			job_title: 'Software Developer',
-			company: 'TELUS',
-			date_year_start: '2019',
-			date_month_start: 'Sept',
-			date_year_end: '2021',
-			date_month_end: 'Apr',
-			currently_working: false,
-			description_items: [
-				'Used Express/Nodejs, React and Cassandra in development of a full stack internal web application to enable self-serve interaction between TELUS team members and a large-scale vendor API ecosystem using Kong',
-				'Developed, deployed and supported microservice APIs defined with OAS2 using Express/Nodejs, Jest, and the OpenShift container platform',
-				'Mentored junior developers in system design, code quality and critical thinking to improve team productivity',
-				'Published a privately scoped NPM package to facilitate standardization of request/response logging and correlation id implementations in other Express/Nodejs microservices'
-			]
-		},
-		{
-			job_title: 'Software Developer',
-			company: 'TELUS',
-			date_year_start: '2016',
-			date_month_start: 'Sep',
-			date_year_end: '2017',
-			date_month_end: 'Apr',
-			currently_working: false,
-			description_items: [
-				'Developed rich web applications with JavaScript and AngularJS to increase visibility and monitoring potential of the company’s software services',
-				'Wrote backend scripts with PHP and SQL to collect and process service status data to better understand downtime events and causes'
-			]
-		},
-		{
-			job_title: 'Software Developer in Test',
-			company: 'Pulse Energy/EnerNOC',
-			date_year_start: '2014',
-			date_month_start: 'Sep',
-			date_year_end: '2015',
-			date_month_end: 'Apr',
-			currently_working: false,
-			description_items: [
-				'Performed regression testing prior to product releases to guard against bugs in the production environment',
-				'Wrote web automation tests with Selenium and Python to reduce manual testing load and promote rapid smoke-testing of the platform'
-			]
-		}
-	];
-	const educationItems = [
-		{
-			degree_title: 'BSc. Computer Science',
-			school: 'Simon Fraser University',
-			date_year_start: '2012',
-			date_year_end: '2018'
-		}
-	];
-	const skillItems = [
-		'Typescript',
-		'Javascript',
-		'ExpressJS',
-		'Angular',
-		'React',
-		'Postgres',
-		'SQL',
-		'AWS',
-		'Git',
-		'Jest',
-		'HTML',
-		'CSS',
-		'OAuth2',
-		'OpenAPI',
-		'API Design',
-		'i18n'
-	];
+	import { resumeContent } from '$lib/resume/content';
 </script>
 
 <div class="paper-container">
 	<div class="resume">
 		<header>
-			<h1>Jordan Toering</h1>
+			<h1>{resumeContent.full_name}</h1>
 			<div class="contact">
-				<div class="profession">Software Developer</div>
-				<div>BC, Canada</div>
+				<div class="profession">{resumeContent.profession_title}</div>
+				<div>{resumeContent.location}</div>
 				<div>
-					<a class="site" target="_blank" href="https://github.com/jordantoering"
-						>github.com/jordantoering</a
+					<a class="site" target="_blank" href={resumeContent.github_details.full_url}
+						>{resumeContent.github_details.short_link}</a
 					>
 				</div>
-				<div>toering3@gmail.com</div>
+				<div>{resumeContent.email}</div>
 			</div>
 		</header>
 
@@ -108,26 +23,26 @@
 				<h2>Work Experience</h2>
 				<div class="color-section-line"></div>
 			</div>
-			{#each positionItems as positionItem}
+			{#each resumeContent.work_experience_items as work_experience_item}
 				<div class="position-item">
 					<div class="position-item-heading">
 						<div class="position-item-job-title">
-							<h2>{positionItem.job_title}</h2>
+							<h2>{work_experience_item.job_title}</h2>
 						</div>
 						<div class="position-item-company-date">
-							<div><h3>{positionItem.company}</h3></div>
+							<div><h3>{work_experience_item.company_name}</h3></div>
 							<div class="position-item-date">
 								<span
-									>{positionItem.date_month_start}
-									{positionItem.date_year_start} - {positionItem.date_month_end}
-									{positionItem.date_year_end}</span
+									>{work_experience_item.date_month_start}
+									{work_experience_item.date_year_start} - {work_experience_item.date_month_end}
+									{work_experience_item.date_year_end}</span
 								>
 							</div>
 						</div>
 					</div>
 					<div class="position-item-description">
 						<ul>
-							{#each positionItem.description_items as item_description}
+							{#each work_experience_item.description_items as item_description}
 								<li>{item_description}</li>
 							{/each}
 						</ul>
@@ -141,18 +56,18 @@
 				<h2>Education</h2>
 				<div class="color-section-line"></div>
 			</div>
-			{#each educationItems as educationItem}
+			{#each resumeContent.education_items as education_item}
 				<div class="education-item">
 					<div class="education-item-row">
 						<div class="education-item-degree">
-							<h2>{educationItem.degree_title}</h2>
+							<h2>{education_item.degree_title}</h2>
 						</div>
 						<div class="education-item-school-year">
 							<div class="education-item-school">
-								<h3>{educationItem.school}</h3>
+								<h3>{education_item.school_name}</h3>
 							</div>
 							<div class="education-item-year">
-								<span>{educationItem.date_year_start} - {educationItem.date_year_end}</span>
+								<span>{education_item.date_year_start} - {education_item.date_year_end}</span>
 							</div>
 						</div>
 					</div>
@@ -166,8 +81,8 @@
 				<div class="color-section-line"></div>
 			</div>
 			<ul class="skills-list">
-				{#each skillItems as skillItem}
-					<li class="skills-list-item">{skillItem}</li>
+				{#each resumeContent.skill_items as skill_item}
+					<li class="skills-list-item">{skill_item}</li>
 				{/each}
 			</ul>
 		</section>
