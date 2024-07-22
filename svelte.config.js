54221ccc71d0fc3737b9a0caff6bfcdae9f212a7
Jordan Toering <toering3@gmail.com>
2024-07-22 14:49:33 -0700

    put back

--- svelte.config.js
+++ svelte.config.js
@@ -4,9 +4,7 @@ import adapter from '@sveltejs/adapter-static';
 const config = {
 	kit: {
 		adapter: adapter({
-			fallback: '404.html',
-			pages: 'build',
-			assets: 'build'
+			fallback: '404.html'
 		})
 	}
 };
